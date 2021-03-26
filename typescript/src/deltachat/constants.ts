730932b4df18f40945db4aa7904313b8fdc9fe86
Simon Laux <mobile.info@simonlaux.de>
2021-03-26 15:52:26 +0100

    update const definition

--- typescript/src/deltachat/constants.ts
+++ typescript/src/deltachat/constants.ts
@@ -4,3 +4,16 @@ export enum Chattype {
     Group = 120,
     Mailinglist = 140,
 }
+
+export const enum MessageStatus {
+    MsgInFresh = 10,
+    MsgInNoticed = 13,
+    MsgInSeen = 16,
+    MsgOutPreparing = 18,
+    MsgOutDraft = 19,
+    MsgOutPending = 20,
+    MsgOutFailed = 24,
+    MsgOutDelivered = 26,
+    /** Seen by other side */
+    MsgOutMdnRcvd = 28,
+}
